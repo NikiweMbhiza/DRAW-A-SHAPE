da0a8f3e616f718a8ca34189e6ba68d42965431f
Nikiw <Nikiw@NIKIWE_MBHIZA>
2024-01-22 14:22:15 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DRAW_A_SHAPE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("    /|");
+
+            Console.WriteLine("   / |");
+
+            Console.WriteLine("  /  |");
+
+            Console.WriteLine(" /   |");
+
+            Console.WriteLine("/    |");
+            Console.WriteLine("-----");
+
+            Console.ReadLine();
+
+        }
+    }
+}
